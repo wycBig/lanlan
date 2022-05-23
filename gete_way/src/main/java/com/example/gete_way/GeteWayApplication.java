6174de9bcf5c2c093eafc14c89e704767e70eb7e
wangyuchuan <18337662715@163.com>
2022-05-23 17:08:09 +0800

    分布式项目架构

---
+++ gete_way/src/main/java/com/example/gete_way/GeteWayApplication.java
@@ -0,0 +1,13 @@
+package com.example.gete_way;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeteWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeteWayApplication.class, args);
+    }
+
+}
---
+++ gete_way/src/test/java/com/example/gete_way/GeteWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gete_way;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeteWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
